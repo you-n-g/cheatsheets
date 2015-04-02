1432454a662ebcf6bd9ab82d0d76d35d861ac42c
Young <afe.young@gmail.com>
2015-04-02 04:24:19 -0400

    add how to use subprocess with stdin, stdout and stderr

---
+++ code_to_copy/backend/python_apps/subprocess.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+import subprocess
+
+process = subprocess.Popen(["XXX_COMMAND", "XXX_ARGS"...], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, unused_err = process.communicate("XXX INPUT")  # this line include input, output, err info;
+code = process.wait()
