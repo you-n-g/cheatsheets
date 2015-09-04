86e5ba117bc453b801be3fd3934428535c33fdaa
Young <afe.young@gmail.com>
2015-09-04 13:55:47 +0800

    vimrc && stl

--- code_to_copy/backend/cplusplus/stl.cpp
+++ code_to_copy/backend/cplusplus/stl.cpp
@@ -44,3 +44,29 @@ min(VALA, VALB);
 
 lower_bound(v.begin(), v.end(), VAL); // first iterator with val >= VAL, otherwith return the position that means v.end()
 upper_bound(v.begin(), v.end(), VAL); // first iterator with val > VAL,
+
+
+//string
+#include<string>
+string s = to_string(INT);
+
+// stoi是C++11支持的标准，  必须加上 -std=c++11 才行
+std::string::size_type sz;
+int I_DEC = stoi(STR_DEC, &sz, 10);
+
+// 这个版本不需要 C++11的支持
+#include<stdlib.h>
+INT = atoi(STRING_OBJ.c_str());
+
+
+// 如何split string, 以','为例
+#include<string>
+#include<iostream>
+#include<sstream> // for stringstream
+string s, item;
+cin >> s;
+stringstream ss(s);
+while (getline(ss, item, ',')) {
+    cout << item << endl;
+}
+
