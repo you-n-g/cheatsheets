889c7b777bed0b8a31e0362ea83b2628f6c76b69
Young <afe.young@gmail.com>
2020-11-03 13:47:42 +0000

    update

--- code_to_copy/backend/thrift_transfer_project/management/commands/transfer_project_server.py
+++ code_to_copy/backend/thrift_transfer_project/management/commands/transfer_project_server.py
@@ -7,15 +7,16 @@ from django.conf import settings
 
 '''
 import daemon #守护进程
-import signal 
+import signal
 signal.signal(signal.SIGCHLD, signal.SIG_IGN) #  如果没有这句话, 将会产程僵尸进程
 '''
 
 from thrift_transfer_project.server import run_server
 
 
-class Command(BaseCommand):                                   
+class Command(BaseCommand):
     help = u"获取项目server"
-    def handle(self, *args, **options):                         
+
+    def handle(self, *args, **options):
         #with daemon.DaemonContext():
         run_server()
