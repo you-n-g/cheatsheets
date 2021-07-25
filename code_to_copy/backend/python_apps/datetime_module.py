369c0dd1496dd57b9152de8680d1115652566e8a
Young <afe.young@gmail.com>
2021-07-25 16:44:11 +0000

    datetime python2 to 3

--- code_to_copy/backend/python_apps/datetime_module.py
+++ code_to_copy/backend/python_apps/datetime_module.py
@@ -2,7 +2,7 @@
 # 将字符串转化为datetime对象
 import datetime
 print(datetime.datetime.strptime('2011-03-07', '%Y-%m-%d'))
-print datetime.datetime.now().isoformat()
+print(datetime.datetime.now().isoformat())
 
 # d1 = datetime.datetime.strptime('2011-03-07', '%Y-%m-%d').date()
 d1 = datetime.datetime(2011, 3, 7).date()
