4a083f520991cb7f5920bfcf0eef299caf673ffb
Young <afe.young@gmail.com>
2024-09-10 10:37:35 +0000

    Python loading .env example

--- language/python/env_interact.py
+++ language/python/env_interact.py
@@ -3,20 +3,46 @@ import os
 
 # Define the shell command as a multi-line string
 shell_command = """
-cat << EOF > .env
+mkdir -p test_subdir/
+
+cat << EOF > test_subdir/.env
 TEST_VAR=123
+EOF
+
+cat << EOF > test_subdir/load.py
+from dotenv import load_dotenv
+load_dotenv()
 EOF
 """
 
 # Execute the shell command
-
+from pathlib import Path
+DIRNAME = Path(__file__).absolute().resolve().parent
 
 import fire
 class Exp:
     def run(self):
-        subprocess.run(shell_command, shell=True, check=True)
+        subprocess.run(shell_command, shell=True, check=True, cwd=DIRNAME)
 
     def test_env(self):
+        """
+        It will work under normal python script.
+
+        .. code-block:: python
+
+            python language/python/env_interact.py run && python language/python/env_interact.py test_env
+
+
+        But it will not work under interactive REPL
+
+        .. code-block:: shell
+
+            cd language/python && python env_interact.py run && python -c "from env_interact import *; Exp().test_env()"
+
+        """
+        import sys
+        sys.path.insert(0, str(DIRNAME))
+        import test_subdir.load
         # NOTE:
         # If you only source the variables
         # - It will fail;
